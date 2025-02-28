87f228202daae14a3bcdba2c5b78cebe4dbcfcaa
GOD-ISOG <isogshadow6@gmail.com>
2025-02-27 22:23:18 -0600

    Page Web

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('moreInfoBtn').addEventListener('click', function() {
+    const moreInfo = document.getElementById('moreInfo');
+    moreInfo.classList.toggle('hidden');
+});
